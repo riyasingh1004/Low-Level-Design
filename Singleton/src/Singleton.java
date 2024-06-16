e68c775a2b215d35ae750b55f89237585e634f62
riyasingh <rsingh99mysteryfan@gmail.com>
2024-06-16 22:33:58 +0530

    Added notes

--- Singleton/src/Singleton.java
+++ Singleton/src/Singleton.java
@@ -1,4 +1,5 @@
 public class Singleton {
+    // volatile -> variable's value can be modified by different threads
     private static volatile Singleton instance;
 
     public String value;
@@ -12,10 +13,14 @@ public class Singleton {
         // exists to prevent race condition between multiple threads that may
         // attempt to get singleton instance at the same time, creating separate
         // instances as a result.
+
+        // initial check avoids synchronization overhead if the instance is already initialized
         Singleton result = instance;
         if (result != null) {
             return result;
         }
+        // even if multiple threads reach the first check at the same time,
+        // only one will enter the synchronized block and create the instance.
         synchronized(Singleton.class) {
             if (instance == null) {
                 instance = new Singleton(value);
